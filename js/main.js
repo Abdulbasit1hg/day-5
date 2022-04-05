b2e4711c53b6bdcf577df5eb693a4862dfee6464
Abdulbasit1hg <99991490+Abdulbasit1hg@users.noreply.github.com>
2022-04-06 03:10:00 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const random = Math.floor(Math.random()* 100) +1;
+
+if (random>1 && random<17){
+    console.log("Maktab yoshi")
+}else if (random>18 && random<=24){
+    console.log("Universitet yoshi")
+}else if (random>=25 && random<=40){
+    console.log("ishlash yoshi")
+}else {
+    console.log ("Nuroniy yoshi")
+}
